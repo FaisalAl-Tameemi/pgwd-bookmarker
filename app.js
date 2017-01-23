3a89ccc5d219af21c4efcb51272134b336f8dad2
Faisal Al-Tameemi <faisal.al.tameemi@gmail.com>
2017-01-22 20:31:39 -0500

    formatting

--- app.js
+++ app.js
@@ -13,35 +13,35 @@ const task = commands.task || commands.t;
 
 // stop if the task hasn't been specified
 if(!task){
-	return console.log("\n Please enter a valid command using '--task [TASK_NAME]' \n");
+  return console.log("\n Please enter a valid command using '--task [TASK_NAME]' \n");
 }
 
 // run the code that matches the task specified
 switch(task){
-	case 'list':
-		console.log('\nHere is a list of all the current bookmarks:\n');
-		console.log(bookmarker.all());
-		console.log('\n');
-		break;
-	case 'new':
-		const new_bookmark = bookmarker.create(commands.title, commands.url);
-		console.log('\nSuccessfully create new bookmark. \n');
-		console.log(new_bookmark);
-		console.log('\n');
-		break;
-	case 'delete':
-		try{
-			bookmarker.deleteById(commands.id);
-			console.log(`\nBookmark with id ${commands.id} has been deleted.\n`);
-		}catch(err){
-			console.log(`\n${err}\n`);
-		}
-		break;
-	case 'delete_all':
-		bookmarker.deleteAll();
-		console.log('\nAll bookmarks have been deleted.\n');
-		break;
-	default:
-		console.log('\n Command not found :( \n');
-		break;
+  case 'list':
+    console.log('\nHere is a list of all the current bookmarks:\n');
+    console.log(bookmarker.all());
+    console.log('\n');
+    break;
+  case 'new':
+    const new_bookmark = bookmarker.create(commands.title, commands.url);
+    console.log('\nSuccessfully create new bookmark. \n');
+    console.log(new_bookmark);
+    console.log('\n');
+    break;
+  case 'delete':
+    try{
+      bookmarker.deleteById(commands.id);
+      console.log(`\nBookmark with id ${commands.id} has been deleted.\n`);
+    }catch(err){
+      console.log(`\n${err}\n`);
+    }
+    break;
+  case 'delete_all':
+    bookmarker.deleteAll();
+    console.log('\nAll bookmarks have been deleted.\n');
+    break;
+  default:
+    console.log('\n Command not found :( \n');
+    break;
 }
