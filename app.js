5c70f310c2cf0589b74b0277eacf709c0256f31e
Faisal Al-Tameemi <faisal.al.tameemi@gmail.com>
2017-02-07 03:51:59 -0500

    removed the solutions

--- app.js
+++ app.js
@@ -2,7 +2,3 @@
 
 // require the bookmarksUtil and create a bookmarker
 const bookmarker = require('./db');
-
-
-bookmarker.removeAll();
-bookmarker.create('test', 'http://google.com');
--- db/index.js
+++ db/index.js
@@ -5,6 +5,8 @@ const lowdb = require('lowdb');
 
 const db = lowdb(`${__dirname}/database.json`);
 
+// If the bookmarks list does not exist in the database files
+// then set its default value to an empty array
 if (!db.has('bookmarks').value()) {
   db.set('bookmarks', []).value();
 };
@@ -14,38 +16,26 @@ const bookmarker = {
   all: function(){
     return db.get('bookmarks').value();
   },
-  // creates a new bookmark and returns it
-  create: function(name, url){
-    const unique_id = shortid.generate();
-    return db
-      .get('bookmarks')
-      .push({
-        id: unique_id,
-        name: name,
-        url: url
-      })
-      .find({
-        id: unique_id
-      })
-      .value();
-  },
-  // returns a bookmark object given its ID, null otherwise
-  find: function(id){
-    return db.get('bookmarks').find({ id }).value();
-  },
-  // deletes a bookmark given its ID
-  remove: function(id){
-    return db.get('bookmarks').remove({ id }).value();
-  },
-  removeAll: function(){
-    db.set('bookmarks', []).value();
-  },
   count: function(){
     return this.all().length;
   }
+  // creates a new bookmark and returns it
+  create: function(name, url){
+
+  },
+  // returns a bookmark object given its ID, null otherwise
+  find: function(id){
+
+  },
+  // deletes a bookmark given its ID
+  remove: function(id){
+
+  },
+  // remove all bookmarks from the database
+  removeAll: function(){
+
+  }
 };
 
-/**
- * Export the object above to other files
- */
+// Export the object above to other files
 module.exports = bookmarker;
